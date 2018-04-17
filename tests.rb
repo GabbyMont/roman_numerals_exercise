d99e7fba779c1cf92cda93f74e46e4a63dd49353
GabbyMont <gemo1236@gmail.com>
2018-04-17 11:09:41 -0400

    tests for 4 and 1 passing

--- tests.rb
+++ tests.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 require_relative 'roman_function.rb'
 
 class Roman_Numerals_test < Minitest::Test
-
 	def test_true_is_true
 		assert_equal(true,true)
+	end
+
+	def test_I_is_1
+		assert_equal("I",roman_numerals(1))
+	end
+
+	def test_IV_is_4
+		assert_equal("IV",roman_numerals(4))
 	end
 end
