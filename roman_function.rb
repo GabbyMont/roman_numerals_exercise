d24cc58b91797cbfe7f1f428d33ff8b2cc504d05
GabbyMont <gemo1236@gmail.com>
2018-04-19 11:09:57 -0400

    break down line 35

--- roman_function.rb
+++ roman_function.rb
@@ -1,36 +1,49 @@
-def roman_numerals(n)
+def divide(x,y)
+	x/y
+end
+
+def mult(a,b)
+	a*b
+end
+
+def roman_numerals(num_to_convert)
 roman_hash = {
-	1000 => "M",  
-    900 => "CM",  
-    500 => "D",  
-    400 => "CD",
-    100 => "C",  
-    90 => "XC",  
-    50 => "L",  
-    40 => "XL",  
-    10 => "X",  
-    9 => "IX",  
+	# 1000 => "M",  
+ #    900 => "CM",  
+ #    500 => "D",  
+ #    400 => "CD",
+ #    100 => "C",  
+ #    90 => "XC",  
+ #    50 => "L",  
+ #    40 => "XL",  
+ #    10 => "X",  
+ #    9 => "IX",  
     5 => "V",  
     4 => "IV", 
 	1 => "I"
 }
-	if n.class == Fixnum
+	if num_to_convert.class == Fixnum
 		roman = ""
-		roman_hash.each do |value,letter|
-			roman << letter*(n/value)
-			n = n % value
+		roman_hash.each do |key,letter| 
+		# p " this is letter #{letter}"
+		# p "this is key #{key}"
+		# p " this is the number converted #{num_to_convert}"
+		return_of_div = divide(num_to_convert,key)
+		# p "Div return #{return_of_div}"
+		mult_str = mult(letter,return_of_div)
+		roman << mult_str
+			# roman << letter*(num_to_convert/key)
+			num_to_convert = num_to_convert % key
+		# p " Roman #{roman}"
 		end
 			roman
 	else
-
-	roman = ""
-	new_array = []
-	roman_convert = 0
-	array = n.split(//)
+		new_array = []
+		roman = 0
+		array = num_to_convert.split(//)
 		array.each do |item|
-			roman_convert += roman_hash.key(item)
-		p "#{roman_convert}"
+			roman += roman_hash.key(item)
 		end
 	end
-return roman_convert
+roman
 end
--- tests.rb
+++ tests.rb
@@ -6,17 +6,41 @@ class Roman_Numerals_test < Minitest::Test
 		assert_equal(true,true)
 	end
 
+	def test_mult_4
+		assert_equal(4,mult(2,2))
+	end
+
+	def test_mult_20
+		assert_equal(20,mult(2,10))
+	end
+
+	def test_div_9
+		assert_equal(3,divide(9,3))
+	end
+
+	def test_divide_8
+		assert_equal(4,divide(8,2))
+	end
+
+	def test_IVIV_mult_2
+		assert_equal("IVIV", mult("IV",2))
+	end
+
+	def test_IVIV_mult_3
+		assert_equal("IVIVIV",mult("IV",3))
+	end
+
 	# def test_I_is_1
 	# 	assert_equal("I",roman_numerals(1))
 	# end
 
-	# def test_IV_is_4
-	# 	assert_equal("IV",roman_numerals(4))
-	# end
-
-	def test_II_is_2
-		assert_equal(2,roman_numerals("II"))
+	def test_IV_is_4
+		assert_equal("IV",roman_numerals(4))
 	end
+
+	# def test_II_is_2
+	# 	assert_equal(2,roman_numerals("II"))
+	# end
 
 	# def test_XXXIII_is_33
 	# 	assert_equal("XXXIII",roman_numerals(33))
